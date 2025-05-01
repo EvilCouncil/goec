be7a2abe76eea828137d2137eb2fd66f72077c22
Jon Heise <j.heise@gmail.com>
2025-04-30 22:32:21 -0700

    adding util

---
+++ pkg/util/encode.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/util/encode_test.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "standard",
+			want: "[\"foo\",\"bar\"]",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(
+				http.MethodGet,
+				"/test",
+				nil,
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rr := httptest.NewRecorder()
+			encode(rr, req, http.StatusOK, []string{"foo", "bar"})
+
+			got := strings.TrimSpace(rr.Body.String())
+			if got != tc.want {
+				t.Errorf("Got: %s, Want: %s", got, tc.want)
+			}
+		})
+	}
+
+}
